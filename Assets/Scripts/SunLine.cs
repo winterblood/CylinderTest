549a623e656d92042f8785be75abcdda980f7a76
Chris Payne <chris@winterblood.com>
2013-10-13 23:48:45 +0100

    Commit test: added moving sun

---
+++ Assets/Scripts/SunLine.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SunLine : MonoBehaviour {
+	
+	public float speed = 100.0f;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		Vector3 pos = transform.position;
+		pos.z += speed * Time.deltaTime;
+		
+		if (pos.z > 1700.0f)
+			pos.z = -1700.0f;
+
+		transform.position = pos;
+	
+	}
+}
